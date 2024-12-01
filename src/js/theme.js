4d4e3658a1ceaa9906c47250fd31d9945e96540d
efran7107 <efran7107@gmail.com>
2024-12-01 06:48:43 -0700

    starting the theme tab

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,31 @@
 const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const root = document.documentElement;
 
-if (isDark) {
-  root.classList.add("dark");
+const theme = localStorage.getItem("theme");
+if (!theme) {
+  if (isDark) {
+    if (!root.classList.contains("dark")) {
+      root.classList.remove("light");
+    }
+    root.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    if (!root.classList.contains("light")) {
+      root.classList.remove("dark");
+    }
+    root.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
+} else {
+  if (theme === "dark") {
+    if (!root.classList.contains("dark")) {
+      root.classList.remove("light");
+    }
+    root.classList.add("dark");
+  } else {
+    if (!root.classList.contains("light")) {
+      root.classList.remove("dark");
+    }
+    root.classList.add("light");
+  }
 }
